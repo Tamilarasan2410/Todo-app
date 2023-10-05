b838750f63ff0c9e27b87c2cfbfbf1869e2b04bf
Tamilarasan2410 <137804059+Tamilarasan2410@users.noreply.github.com>
2023-10-05 21:02:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer, useState } from "react";
+
+const initialtodos = [
+  // {
+  //   id: 1,
+  //   Tittle: "todo1",
+  //   Completed: false,
+  // },
+  // {
+  //   id: 2,
+  //   Tittle: "todo2",
+  //   Completed: false,
+  // },
+  // {
+  //   id: 3,
+  //   Tittle: "todo3",
+  //   Completed: false,
+  // },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Completed":
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, Completed: !todo.Completed };
+        }
+        return todo;
+      });
+
+    case "add":
+      if (state.length === 0)
+        return [...state, { id: 1, Tittle: action.Tittle, Completed: false }];
+      else
+        return [
+          ...state,
+          {
+            id: state[state.length - 1].id + 1,
+            Tittle: action.Tittle,
+            Completed: false,
+          },
+        ];
+    default:
+      return state;
+
+    case "Delete":
+      return state.filter((todo) => todo.id !== action.id);
+  }
+};
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, initialtodos);
+  const [newtodo, setnewtodo] = useState("");
+  const handleclick = () => {
+    dispatch({ type: "add", Tittle: newtodo });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO APP</h1>
+      <input
+        type="Text"
+        placeholder=" Add Todo"
+        onChange={(e) => setnewtodo(e.target.value)}
+      />
+      <button onClick={handleclick}>Add</button>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <h3>{todo.Tittle}</h3>
+            <button
+              onClick={() => dispatch({ type: "Completed", id: todo.id })}
+            >
+              {todo.Completed ? "Completed" : "Not Completed"}
+            </button>
+            <button onClick={() => dispatch({ type: "Delete", id: todo.id })}>
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
